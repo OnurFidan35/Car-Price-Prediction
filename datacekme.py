7cc811eccbef335752bb1d131464e4cd491021e8
Onur Fidan <230260047@gmail.com>
2024-12-31 16:42:27 +0300

    Added ilanlar.csv

--- datacekme.py
+++ datacekme.py
@@ -17,6 +17,8 @@ class Cars():
     def __init__(self):
         self.veri_cek()
     
+    
+    
     def veri_cek(self):
         driver.get("https://www.arabam.com/ikinci-el/otomobil?take=50&view=List")
         WebDriverWait(driver,10).until(
@@ -24,6 +26,12 @@ class Cars():
             EC.presence_of_element_located((By.CLASS_NAME,'listing-table-wrapper'))
             
         )
+
+        def ensure_all_columns(data):
+            max_length = max(len(v) for v in data.values())
+            for key in data:
+                if len(data[key]) < max_length:
+                    data[key].append(np.nan)
      
         
         data={
@@ -52,6 +60,7 @@ class Cars():
 
 
         for i in range(1,51):
+            print("Sayfa",i,"\n")
             if i==1:
                 driver.get("https://www.arabam.com/ikinci-el/otomobil?take=50&view=List")
                 sleep(3)
@@ -71,36 +80,32 @@ class Cars():
                   continue
             for link in car_links_list:
                 driver.get(link)
-                sleep(3)
+                sleep(1)
                 feautures=driver.find_element(By.CLASS_NAME,'product-detail-wrapper').find_element(By.CLASS_NAME,'product-properties-container').find_element(By.CLASS_NAME,'product-properties')
                 price=feautures.find_element(By.CLASS_NAME,'product-properties-details-header').find_element(By.CLASS_NAME,'product-price').text
                 others=feautures.find_elements(By.CLASS_NAME,'property-item')
-                #Yakıt deposu ve Ort. Yakıt Tüketimi opsiyonel olduğundan her zaman olmuyor bu yüzden kontrol ettim ve olmadığı zaman nan yazdırdım
-                fuel_tank_exists=False
-                avg_fuel_exists=False
+                
                 for feauture in others:
                     try:
                         feauture_value=feauture.find_element(By.CLASS_NAME,'property-value').text
                         feauture_key=feauture.find_element(By.CLASS_NAME,'property-key').text.strip()
-                        if feauture_key=="Yakıt Deposu":
-                            fuel_tank_exists=True
-                        elif feauture_key=="Ort. Yakıt Tüketimi":
-                            avg_fuel_exists=True
                         data[feauture_key].append(feauture_value)
                     except:
                         continue
-                if fuel_tank_exists==False:
-                    data["Yakıt Deposu"].append(np.nan)
-                if avg_fuel_exists==False:
-                    data["Ort. Yakıt Tüketimi"].append(np.nan)
+                    
+                
 
-                data["Fiyat"].append(price)    
+                data["Fiyat"].append(price)   
+                ensure_all_columns(data)
+                
+                
+             
                 
         
         df=pd.DataFrame(data) 
 
 
-        df.to_csv("ilanlar.csv", index=False, encoding="utf-8-sig")
+        df.to_csv("ilanlar.csv",mode='a', index=False, header=False)
         print("bitti")
         driver.quit()
         
